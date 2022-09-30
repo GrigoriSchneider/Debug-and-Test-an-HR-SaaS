0e9029e8b390049bea9f7c138311e44a506a6d05
GrigoriSchneider <grigorischneider@gmail.com>
2022-09-30 09:03:53 +0200

    fixed some spelling mistakes & added the presentation powerpoint

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,16 +3,47 @@ import BillsUI from '../views/BillsUI.js';
 import { bills } from '../fixtures/bills.js';
 import Bills from '../containers/Bills.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
-import { ROUTES } from '../constants/routes.js';
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js';
 import userEvent from '@testing-library/user-event';
 import firebase from '../__mocks__/firebase.js';
+import Firestore from '../app/Firestore';
+import Router from '../app/Router';
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on Bills Page', () => {
+    const pathname = ROUTES_PATH['Bills'];
+    let onNavigate;
+    beforeEach(() => {
+      onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      Object.defineProperty(window, 'localStorage', {
+        value: localStorageMock,
+      });
+
+      Object.defineProperty(window, 'location', {
+        value: {
+          hash: pathname,
+        },
+      });
+      window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }));
+    });
+
     test('Then bill icon in vertical layout should be highlighted', () => {
-      const html = BillsUI({ data: [] });
-      document.body.innerHTML = html;
-      //to-do write expect expression
+      jest.mock('../app/Firestore');
+      // Mock - parameters for Behavior Driven Development (BDD)  Firebase & data fetching
+      Firestore.bills = () => ({ bills, get: jest.fn().mockResolvedValue() });
+
+      // Get the HTML content of the current document:
+      // Change the HTML content of the current document (will overwrite all existing HTML elements inside <body>):
+      document.body.innerHTML = `<div id="root"></div>`;
+
+      // Router init to get actives CSS classes
+      Router();
+      expect(
+        // "icon-window" must contain the class "active-icon"
+        screen.getByTestId('icon-window').classList.contains('active-icon')
+      ).toBe(true);
     });
     test('Then bills should be ordered from earliest to latest', () => {
       const html = BillsUI({ data: bills });
@@ -101,8 +132,8 @@ describe('Given I am connected as an employee', () => {
 });
 
 // intergration test GET
-describe('Given I am user Connected as emmployee', () => {
-  describe('When I navigate to dashoboard', () => {
+describe('Given I am user Connected as employee', () => {
+  describe('When I navigate to dashboard', () => {
     test('should fetch bills from mock API GET', async () => {
       const getSpy = jest.spyOn(firebase, 'get');
       const bills = await firebase.get();
